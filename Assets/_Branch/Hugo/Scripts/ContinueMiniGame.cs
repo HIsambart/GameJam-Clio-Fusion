3035e98509834e7eb126da978a599400ddc70a34
Neiflheim <isambart.hugo0912@gmail.com>
2026-04-11 10:46:32 +0200

    Added TokamakManager, MiniGame pressure and temperature, DebrisHandler

---
+++ Assets/_Branch/Hugo/Scripts/ContinueMiniGame.cs
@@ -0,0 +1,20 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace _Branch.Hugo.Scripts
+{
+    public abstract class ContinueMiniGame : MonoBehaviour, IInteractable
+    {
+        [Header("===== DATA =====")]
+        public Vector2 RangeLevel;
+        public float MinimumLevel;
+        
+        [Range(0f, 100f)] public  float CurrentLevel;
+
+        [Header("===== SETTINGS =====")]
+        public float DecreaseSpeed;
+        public float AddAmount;
+        
+        public abstract void Interact();
+    }
+}
---
+++ Assets/_Branch/Hugo/Scripts/TokamakManager.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+namespace _Branch.Hugo.Scripts
+{
+    public class TokamakManager : MonoBehaviourSingleton<TokamakManager>
+    {
+        [Header("===== DATA =====")]
+        [Header("LEVEL")]
+        public Vector2 RangeStability;
+        public float LooseStabilityLevel;
+        public float WinStabilityLevel;
+        
+        [Range(0f, 100f)] public float CurrentStabilityLevel;
+        
+        [Header("Speed")]
+        public float IncreaseSpeed;
+        public float DecreaseSpeed;
+        
+        [Header("===== REFERENCES =====")]
+        public List<ContinueMiniGame> ContinueMiniGames = new();
+
+        private void Awake()
+        {
+            CurrentStabilityLevel = RangeStability.y /2;
+        }
+
+        private void Update()
+        {
+            foreach (var miniGame in ContinueMiniGames)
+            {
+                if (miniGame.CurrentLevel >= miniGame.MinimumLevel)
+                {
+                    CurrentStabilityLevel += IncreaseSpeed* Time.deltaTime;
+                }
+                else
+                {
+                    CurrentStabilityLevel -= DecreaseSpeed* Time.deltaTime;
+                }
+            }
+
+            if (CurrentStabilityLevel > WinStabilityLevel)
+            {
+                Debug.Log("WIN");
+            }
+            else if (CurrentStabilityLevel < LooseStabilityLevel)
+            {
+                Debug.Log("LOOSE");
+            }
+        }
+    }
+}
